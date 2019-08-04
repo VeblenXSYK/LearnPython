607f81bd7f23d68abafc8e4f9115c35cd7474868
VeblenXSYK <1207316368@qq.com>
2019-08-04 16:14:06 +0800

    添加测试__init__.py作用的代码

---
+++ recipes/__init__/mypackage/__init__.py
@@ -0,0 +1,7 @@
+import os
+
+print("You have imported mypackage")
+print "Cur work dir: " + os.getcwd()
+
+__all__ = ['subpackage_1', 'subpackage_2']
+
---
+++ recipes/__init__/mypackage/subpackage_1/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+# 绝对路径导入
+# from mypackage.subpackage_1 import test11, test12
+# 相对路径导入
+from . import test11, test12
+
+__all__ = ['test11', 'test12']
